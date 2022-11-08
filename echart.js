e9353e79c2dfebfb6f910af99a328644740920f4
Darshan Savani <69293489+Savani5700@users.noreply.github.com>
2022-11-08 13:45:53 +0530

    1

--- echart.js
+++ echart.js
@@ -1,1 +1,95 @@
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+  
+  (function () {
+  
+    //Chart Block in HTML Darshan Savani
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+  <style>
+  </style>
+  <div id="root" style="width: 100%; height: 100%;">
+  </div>
+  `
+    //Main JS Class holds methods to be called
+    class SamplePrepared extends HTMLElement {
+      constructor() {
+  
+        //call SAC DOM Super method to get shadow DOM information
+        super()
+  
+        //Get shadow DOM informations
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+  
+        //Set HTML block in shadow DOM of SAC
+        this._root = this._shadowRoot.getElementById('root')
+  
+        //_props object is used to hold properties infosrmation
+        this._props = {}
+  
+        //Call render() method to plot chart
+        this.render()
+  
+      }
+  
+      onCustomWidgetResize(width, height) {
+        this.render()
+      }
 
+  
+      async render() {
+        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js');
+  
+        const chart = echarts.init(this._root)
+        var data = [];
+  
+        for (let i = 0; i <= 360; i++) {
+          let t = (i / 180) * Math.PI;
+          let r = Math.sin(2 * t) * Math.cos(2 * t);
+          data.push([r, i]);
+        }
+        const option = {
+          title: {
+            text: 'Two Value-Axes in Polar'
+          },
+          legend: {
+            data: ['line']
+          },
+          polar: {
+            center: ['50%', '54%']
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross'
+            }
+          },
+          angleAxis: {
+            type: 'value',
+            startAngle: 0
+          },
+          radiusAxis: {
+            min: 0
+          },
+          series: [
+            {
+              coordinateSystem: 'polar',
+              name: 'line',
+              type: 'line',
+              showSymbol: false,
+              data: data
+            }
+          ],
+          animationDuration: 2000
+        };
+  
+      }
+    }
+    customElements.define('com-sap-sample-echart-prepared', SamplePrepared)
+  })()
+  
+  
