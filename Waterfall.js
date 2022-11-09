48d2b81be75186d6486926e8475fa6ac5dd50975
Darshan Savani <69293489+Savani5700@users.noreply.github.com>
2022-11-09 09:21:19 +0530

    1

--- Waterfall.js
+++ Waterfall.js
@@ -1,11 +1,11 @@
 
 var getScriptPromisify = (src) => {
     return new Promise(resolve => {
-      $.getScript(src, resolve)
+        $.getScript(src, resolve)
     })
-  }
-  
-  (function () {
+}
+
+(function () {
     const prepared = document.createElement('template')
     prepared.innerHTML = `
         <style>
@@ -14,94 +14,94 @@ var getScriptPromisify = (src) => {
         </div>
       `
     class SamplePrepared extends HTMLElement {
-      constructor () {
-        super()
-  
-        this._shadowRoot = this.attachShadow({ mode: 'open' })
-        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
-  
-        this._root = this._shadowRoot.getElementById('root')
-  
-        this._props = {}
-  
-        this.render()
-      }
-  
-      onCustomWidgetResize (width, height) {
-        this.render()
-      }
-  
-      async render () {
-        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
-  
-        const chart = echarts.init(this._root)
-        var data = [];
-        
-        const option = {
-          title: {
-            text: 'Waterfall Chart',
-            subtext: 'Living Expenses in Shenzhen'
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'shadow'
-            },
-            formatter: function (params) {
-              var tar = params[1];
-              return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis: {
-            type: 'category',
-            splitLine: { show: false },
-            data: ['Total', 'Rent', 'Utilities', 'Transportation', 'Meals', 'Other']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              name: 'Placeholder',
-              type: 'bar',
-              stack: 'Total',
-              itemStyle: {
-                borderColor: 'transparent',
-                color: 'transparent'
-              },
-              emphasis: {
-                itemStyle: {
-                  borderColor: 'transparent',
-                  color: 'transparent'
-                }
-              },
-              data: [0, 1700, 1400, 1200, 300, 0]
-            },
-            {
-              name: 'Life Cost',
-              type: 'bar',
-              stack: 'Total',
-              label: {
-                show: true,
-                position: 'inside'
-              },
-              data: [2900, 1200, 300, 200, 900, 300]
-            }
-          ]
-        };
-        
+        constructor() {
+            super()
 
-        chart.setOption(option)
-  
-      }
+            this._shadowRoot = this.attachShadow({ mode: 'open' })
+            this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+            this._root = this._shadowRoot.getElementById('root')
+
+            this._props = {}
+
+            this.render()
+        }
+
+        onCustomWidgetResize(width, height) {
+            this.render()
+        }
+
+        async render() {
+            await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+
+            const chart = echarts.init(this._root)
+            var data = [];
+
+            const option = {
+                title: {
+                    text: 'Waterfall Chart',
+                    subtext: 'Living Expenses in Shenzhen'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    },
+                    formatter: function (params) {
+                        var tar = params[1];
+                        return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    splitLine: { show: false },
+                    data: ['Total', 'Rent', 'Utilities', 'Transportation', 'Meals', 'Other']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: 'Placeholder',
+                        type: 'bar',
+                        stack: 'Total',
+                        itemStyle: {
+                            borderColor: 'transparent',
+                            color: 'transparent'
+                        },
+                        emphasis: {
+                            itemStyle: {
+                                borderColor: 'transparent',
+                                color: 'transparent'
+                            }
+                        },
+                        data: [0, 1700, 1400, 1200, 300, 0]
+                    },
+                    {
+                        name: 'Life Cost',
+                        type: 'bar',
+                        stack: 'Total',
+                        label: {
+                            show: true,
+                            position: 'inside'
+                        },
+                        data: [2900, 1200, 300, 200, 900, 300]
+                    }
+                ]
+            };
+
+
+            chart.setOption(option)
+
+        }
     }
     customElements.define('com-sap-sample-echart-prepared', SamplePrepared)
-  })()
-  
-  
+})()
+
+
--- echart.js
+++ echart.js
@@ -1,111 +1,87 @@
 var getScriptPromisify = (src) => {
-    return new Promise(resolve => {
-      $.getScript(src, resolve)
-    })
-  }
-  
-  (function () {
-    const prepared = document.createElement('template')
-    prepared.innerHTML = `
+  return new Promise(resolve => {
+    $.getScript(src, resolve)
+  })
+}
+
+(function () {
+  const prepared = document.createElement('template')
+  prepared.innerHTML = `
         <style>
         </style>
         <div id="root" style="width: 100%; height: 100%;">
         </div>
       `
-    class SamplePrepared extends HTMLElement {
-      constructor () {
-        super()
-  
-        this._shadowRoot = this.attachShadow({ mode: 'open' })
-        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
-  
-        this._root = this._shadowRoot.getElementById('root')
-  
-        this._props = {}
-  
-        this.render()
-      }
-  
-      onCustomWidgetResize (width, height) {
-        this.render()
-      }
-  
-      async render () {
-        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
-  
-        const chart = echarts.init(this._root)
-        var data = [];
-  
-        // for (var i = 0; i <= 360; i++) {
-        //   var t = (i / 180) * Math.PI;
-        //   var r = Math.sin(2 * t) * Math.cos(2 * t);
-        //   data.push([r, i]);
-        // }
-        const option = {
-          title: {
-            text: 'Waterfall Chart',
-            subtext: 'Living Expenses in Shenzhen'
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'shadow'
-            },
-            formatter: function (params) {
-              var tar = params[1];
-              return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis: {
-            type: 'category',
-            splitLine: { show: false },
-            data: ['Total', 'Rent', 'Utilities', 'Transportation', 'Meals', 'Other']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              name: 'Placeholder',
-              type: 'bar',
-              stack: 'Total',
-              itemStyle: {
-                borderColor: 'transparent',
-                color: 'transparent'
-              },
-              emphasis: {
-                itemStyle: {
-                  borderColor: 'transparent',
-                  color: 'transparent'
-                }
-              },
-              data: [0, 1700, 1400, 1200, 300, 0]
-            },
-            {
-              name: 'Life Cost',
-              type: 'bar',
-              stack: 'Total',
-              label: {
-                show: true,
-                position: 'inside'
-              },
-              data: [2900, 1200, 300, 200, 900, 300]
-            }
-          ]
-        };
-        
+  class SamplePrepared extends HTMLElement {
+    constructor() {
+      super()
 
-        chart.setOption(option)
-  
-      }
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+
+      this._props = {}
+
+      this.render()
     }
-    customElements.define('com-sap-sample-echart-prepared', SamplePrepared)
-  })()
-  
-  
+
+    onCustomWidgetResize(width, height) {
+      this.render()
+    }
+
+    async render() {
+      await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+
+      const chart = echarts.init(this._root)
+      var data = [];
+
+      for (var i = 0; i <= 360; i++) {
+        var t = (i / 180) * Math.PI;
+        var r = Math.sin(2 * t) * Math.cos(2 * t);
+        data.push([r, i]);
+      }
+
+      const option = {
+        title: {
+          text: 'Two Value-Axes in Polar'
+        },
+        legend: {
+          data: ['line']
+        },
+        polar: {
+          center: ['50%', '54%']
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        angleAxis: {
+          type: 'value',
+          startAngle: 0
+        },
+        radiusAxis: {
+          min: 0
+        },
+        series: [
+          {
+            coordinateSystem: 'polar',
+            name: 'line',
+            type: 'line',
+            showSymbol: false,
+            data: data
+          }
+        ],
+        animationDuration: 2000
+      };
+
+      chart.setOption(option)
+
+    }
+  }
+  customElements.define('com-sap-sample-echart-prepared', SamplePrepared)
+})()
+
+
