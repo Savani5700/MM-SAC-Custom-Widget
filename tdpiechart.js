b26d4221ae0330f2a30cf7f766c427b381a4bd19
Darshan Savani <69293489+Savani5700@users.noreply.github.com>
2022-11-29 14:32:40 +0530

    Create tdpiechart.js

---
+++ tdpiechart.js
@@ -0,0 +1,119 @@
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+  
+  (function () {
+    const prepared = document.createElement('template')
+    prepared.innerHTML = `
+  <style>
+  </style>
+  <div id="root" style="width: 100%; height: 100%;">
+  </div>
+  `
+    class SamplePrepared extends HTMLElement {
+      constructor() {
+        super()
+  
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(prepared.content.cloneNode(true))
+  
+        this._root = this._shadowRoot.getElementById('root')
+  
+        this._props = {}
+  
+        // this.render(this._resultset)
+      }
+  
+      // onCustomWidgetResize(width, height) {
+      //   // this.render()
+      // }
+  
+      async render(resultset1) {
+        await getScriptPromisify('https://cdn.amcharts.com/lib/4/core.js');
+        await getScriptPromisify('https://cdn.amcharts.com/lib/4/themes/animated.js');
+        await getScriptPromisify('https://cdn.amcharts.com/lib/4/charts.js');
+  
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+        
+        // Create chart instance
+        var chart = am4core.create(this._root, am4charts.XYChart);
+  
+        console.log(resultset1);
+
+        var data = [];
+        // Add data
+
+        for(var i=0; i<resultset1.length;i = i+3){
+
+          var a = {
+            year : resultset1[i]["Invoice_Date"].description,
+            profit : resultset1[i]["@MeasureDimension"].rawValue,
+            sales : resultset1[i+1]["@MeasureDimension"].rawValue,
+            cogs : resultset1[i+2]["@MeasureDimension"].rawValue
+          }
+
+          data.push(a);
+
+        }
+        // console.log(data);
+
+        chart.data = data;
+  
+        // Create axes
+        
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "year";
+        categoryAxis.title.text = "Profit & Loss Summary";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 20
+        categoryAxis.renderer.cellStartLocation = 0.1;
+        categoryAxis.renderer.cellEndLocation = 0.9;
+        categoryAxis.renderer.grid.template.strokeWidth = 0.2;
+  
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.min = 0;
+        valueAxis.title.text = "Sales, COGS";
+        var valueAxis1 = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis1.renderer.opposite = true;
+        valueAxis1.title.text = "Profit Loss";
+        valueAxis1.renderer.grid.template.strokeWidth = 0;
+        // Configure number formatter
+        valueAxis.numberFormatter.numberFormat = '$#.#M';
+  
+        // Create series
+        function createSeries(field, name, stacked, yaxis, color) {
+          var series = chart.series.push(new am4charts.ColumnSeries());
+          series.dataFields.valueY = field;
+          series.yAxis = yaxis;
+          series.dataFields.categoryX = "year";
+          series.name = name;
+          series.columns.template.tooltipText = "{name}: [bold]{valueY}[/]";
+          series.stacked = stacked;
+          series.columns.template.width = am4core.percent(95);
+  
+          series.columns.template.adapter.add("fill", function (fill, target) {
+            if (target.dataItem && (target.dataItem.valueY < 0)) {
+              return am4core.color("#ff4a4a");
+            } else {
+              return am4core.color(color);
+            }
+          });
+          series.columns.template.width = am4core.percent(100);
+        }
+  
+        createSeries("profit", "Profit", false, valueAxis, "#204666");
+        createSeries("sales", "Sales", true, valueAxis, "#f3b026");
+        createSeries("cogs", "COGS", true, valueAxis1, "#02a264");
+  
+        // Add legend
+        /* chart.legend = new am4charts.Legend(); */
+  
+      }
+    }
+    customElements.define('com-sap-sample-tdpiechart-prepared', SamplePrepared)
+  })()
+  
