f2d678f67acf70e0009febfc6e8e8e9f83e92921
Darshan Savani <69293489+Savani5700@users.noreply.github.com>
2022-11-30 09:46:32 +0530

    1

--- tdpiechart.js
+++ tdpiechart.js
@@ -9,7 +9,7 @@ var getScriptPromisify = (src) => {
   prepared.innerHTML = `
 <style>
 </style>
-<div id="root" style="padding-top: 20px; padding-right: 20px; padding-bottom: 20px; padding-left: 20px;">
+<div id="root" style="margin-top: 20px; margin-right: 20px; margin-bottom: 20px; margin-left: 20px;">
 </div>
 `
   class SamplePrepared extends HTMLElement {
