b07c6eadb3ea466eadf93562e9a466735fcfc563
Darshan Savani <69293489+Savani5700@users.noreply.github.com>
2023-02-15 15:33:03 +0530

    1

--- Varriance_Indicators.js
+++ Varriance_Indicators.js
@@ -147,5 +147,5 @@ async render() {
 
 }
 }
-customElements.define('com-sap-sample-VarrianceIndicators-prepared', SamplePrepared)
+customElements.define('com-sap-sample-Varriance-prepared', SamplePrepared)
 })()
